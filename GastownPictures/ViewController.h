5341b93335a9c6d1539296d6a2558c62d5f50cb2
imackinn <imackinn@alumni.uwaterloo.ca>
2015-06-04 08:43:25 -0700

    inital project commit

---
+++ GastownPictures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GastownPictures
+//
+//  Created by Ian MacKinnon on 2015-06-04.
+//  Copyright (c) 2015 Ian MacKinnon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GastownPictures/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GastownPictures
+//
+//  Created by Ian MacKinnon on 2015-06-04.
+//  Copyright (c) 2015 Ian MacKinnon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

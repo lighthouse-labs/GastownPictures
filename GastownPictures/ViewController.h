78074046d3d52034a3d09c27514dc3b79ad76354
imackinn <imackinn@alumni.uwaterloo.ca>
2015-06-04 08:49:59 -0700

    add Mapview and Cocoapods

--- GastownPictures/ViewController.h
+++ GastownPictures/ViewController.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface ViewController : UIViewController
 
+
+@property(nonatomic, weak) IBOutlet MKMapView *gastownMapView;
 
 @end
 
